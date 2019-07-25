0331a4fc6b1efa991e226d7b33732431dc1a605c
defrox <defrostdj@hotmail.com>
2019-07-25 20:20:57 +0200

    1 file modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
-import solidGauge from 'highcharts/modules/solid-gauge.src';
 import more from 'highcharts/highcharts-more.src';
+import solidGauge from 'highcharts/modules/solid-gauge.src';
 import { AppComponent } from './app.component';
 
 export function highchartsModules() {
   // apply Highcharts Modules to this array
-  return [solidGauge, more];
+  return [more, solidGauge];
 }
 
 
