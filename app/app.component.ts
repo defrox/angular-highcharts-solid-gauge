b151359708597b9c1c8d853802ca1cab35087258
defrox <defrostdj@hotmail.com>
2019-07-29 20:34:47 +0200

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,71 @@ export class AppComponent {
   gauge: Chart;
 
   ngOnInit() {
+        this.gaugeNoFormat = new Chart({
+        chart: {
+            type: 'solidgauge',
+            height: '100%',
+            width: 350,
+            backgroundColor: 'transparent'
+        },
+        credits: {enabled: false},
+        title: {
+            text: 'No format',
+            y: 250,
+            style: {'font-family': 'Muli, Helvetica Neue, Arial, sans-serif', 'font-size': '36px'}
+        },
+        pane: {
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: 'white',
+                innerRadius: '60%',
+                outerRadius: '90%',
+                shape: 'arc',
+                borderColor: 'transparent',
+            }
+        },
+        tooltip: {
+            enabled: false
+        },
+        yAxis: {
+            stops: [
+                [0.5, 'red'],
+                [1, 'green']
+            ],
+            length: 5,
+            lineWidth: 0,
+            minorTicks: false,
+            tickAmount: 0,
+            tickColor: 'transparent',
+            labels: {
+                enabled: false,
+            },
+            min: -100,
+            max: 100,
+            plotBands: [
+                { from: -100, to: 0, color: 'red', outerRadius: '132'},
+                { from: 0, to: 100, color: 'green', outerRadius: '132'},
+            ]
+        },
+        plotOptions: {
+            solidgauge: {
+                threshold: 0,
+                dataLabels: {
+                    style: {'fontSize': '36px', 'font-family': 'Muli, Helvetica Neue, Arial, sans-serif', 'fontWeight': 'light'},
+                    y: -50,
+                    borderWidth: 0,
+                    formatter: centeredGaugeNoFormatter,
+                }
+            }
+        },
+        series: [
+            {
+                data: [-10]
+            }
+        ]
+    });
+
     this.gauge = new Chart({
         chart: {
             type: 'solidgauge',
@@ -18,7 +83,7 @@ export class AppComponent {
         },
         credits: {enabled: false},
         title: {
-            text: 'Title',
+            text: 'With format',
             y: 250,
             style: {'font-family': 'Muli, Helvetica Neue, Arial, sans-serif', 'font-size': '36px'}
         },
@@ -78,4 +143,8 @@ export class AppComponent {
 
 export function centeredGaugeFormatter(): number | string {
     return Math.abs((this.y - 50) * 2);
+}
+
+export function centeredGaugeNoFormatter(): number | string {
+    return Math.abs(this.y);
 }
